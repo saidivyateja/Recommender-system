00fea31ee03042089f8ea998cb327b47f7fc242a
Konda Sai Divya Teja <69079789+saidivyateja@users.noreply.github.com>
2023-04-28 19:05:21 -0400

    Update animeadept.py

--- animeadept.py
+++ animeadept.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 import time
 import ast
-import requests
-import matplotlib.pyplot as plt
-import plotly.express as px
-import seaborn as sns
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MultiLabelBinarizer, StandardScaler
 from sklearn.feature_extraction.text import CountVectorizer
